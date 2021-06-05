efce24ae49e9f6ded963c74bae3034eebdc1ab85
yeafla530 <yeafla530@naver.com>
2021-06-06 02:17:00 +0900

    210606 | 트리의 부모 찾기

--- python/그래프탐색/트리의부모찾기.py
+++ python/그래프탐색/트리의부모찾기.py
@@ -1,28 +1,24 @@
-from collections import deque
+# 부모에 있는 자식들을 구하는 것은 bfs
 
+from collections import deque
 n = int(input())
 graph = [[] for _ in range(n+1)]
-check = [0 for _ in range(n+1)]
-answer = [0] * (n+1)
-print(graph, check, answer)
+parent = [0] * (n+1)
 for _ in range(n-1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-# 부모를 찾는 문제이므로 모든 자식을 탐색하는
-# bfs를 쓰는게 좋음
 queue = deque([1])
-
+parent[1] = -1
 while queue:
-    parent = queue.popleft()
-    for a in graph[parent]:
-        if check[a] == 0:
-            answer[a] = parent
-            queue.append(a)
-            print(queue)
-            check[a] = 1
-            
-for i in range(2, n+1):
-    print(answer[i])
-        
+    v = queue.popleft()
+    # print(graph[v])
+    for i in graph[v]:
+        if parent[i] == 0:
+            queue.append(i)
+            parent[i] = v 
+
+# print(parent[2:])
+for i in parent[2:]:
+    print(i)

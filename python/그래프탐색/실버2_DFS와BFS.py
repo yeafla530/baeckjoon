91d27f553666e12219a520b8ee5652e9a1ae3e10
yeafla530 <yeafla530@naver.com>
2021-06-06 01:54:07 +0900

    210606 | DFS와 BFS

--- python/그래프탐색/실버2_DFS와BFS.py
+++ python/그래프탐색/실버2_DFS와BFS.py
@@ -1,34 +1,34 @@
-from collections import deque
-
-
 n, m, v = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 visited_dfs = [0] * (n+1)
 visited_bfs = [0] * (n+1)
+
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(graph, v, visited_dfs):
-    visited_dfs[v] = 1
+def dfs(graph, v, visited):
+    visited[v] = 1
     print(v, end=' ')
     for i in sorted(graph[v]):
-        if visited_dfs[i] == 0:
-            dfs(graph, i, visited_dfs)
+        if visited[i] == 0:
+            dfs(graph, i, visited) 
 
-def bfs(graph, v, visited_bfs):
+from collections import deque
+def bfs(graph, v, visited):
     queue = deque([v])
-    visited_bfs[v] = 1
+    visited[v] = 1
     while queue:
         v = queue.popleft()
         print(v, end=' ')
         for i in sorted(graph[v]):
-            if visited_bfs[i] == 0:
-                visited_bfs[i] = 1
+            if visited[i] == 0:
+                visited[i] = 1
                 queue.append(i)
+        
 
 
-dfs(graph, v, visited_dfs)
-print('')
-bfs(graph, v, visited_bfs)
+dfs(graph, 1, visited_dfs)
+print()
+bfs(graph, 1, visited_bfs)

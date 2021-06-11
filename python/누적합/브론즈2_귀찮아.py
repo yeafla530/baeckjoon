c412354d9a14458ee52c71c769e0ee1cee2a8383
yeafla530 <yeafla530@naver.com>
2021-06-12 00:48:56 +0900

    210612 | 2차배열의 합

--- python/누적합/브론즈2_귀찮아.py
+++ python/누적합/브론즈2_귀찮아.py
@@ -5,8 +5,8 @@ mysum.append(tmp[0]) #첫번째 수 넣기
 ans=0
 for i in range(1,n): # 이전값이랑 다음값 더함
     mysum.append(mysum[i-1]+tmp[i])
-print(mysum)
+
 for i in range(n): # 정답에 
-    print(tmp[i], mysum[n-1], mysum[i], mysum[n-1]-mysum[i])
+    # print(tmp[i], mysum[n-1], mysum[i], mysum[n-1]-mysum[i])
     ans+=tmp[i]*(mysum[n-1]-mysum[i])
 print(ans)
---
+++ python/누적합/실버2_출석체크.py
@@ -0,0 +1,3 @@
+n, k, q, m = map(int, input().split())
+k_lst = list(map(int, input().split()))
+q_lst = list(map(int, input().split()))

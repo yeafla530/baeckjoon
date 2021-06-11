7bff37de62366b1f592e4f0996d8db19ff74cef5
yeafla530 <yeafla530@naver.com>
2021-06-12 01:46:01 +0900

    수정

--- python/누적합/브론즈2_귀찮아.py
+++ python/누적합/브론즈2_귀찮아.py
@@ -1,12 +1,12 @@
-n=int(input())
-tmp=list(map(int, input().split()))
-mysum=[]
-mysum.append(tmp[0]) #첫번째 수 넣기
-ans=0
-for i in range(1,n): # 이전값이랑 다음값 더함
-    mysum.append(mysum[i-1]+tmp[i])
+n = int(input())
+temp = list(map(int, input().split()))
+num = []
+num.append(temp[0])
+for i in range(1, n):
+    num.append(num[i-1] + temp[i])
 
-for i in range(n): # 정답에 
-    # print(tmp[i], mysum[n-1], mysum[i], mysum[n-1]-mysum[i])
-    ans+=tmp[i]*(mysum[n-1]-mysum[i])
-print(ans)
+answer = 0
+for i in range(n):
+    answer += temp[i] * (num[n-1] - num[i])
+
+print(answer)
